9c05c6a3290a384831d19683ed8cfaa46d3507c7
chrisgclayton <chrisclayton@outlook.com>
2020-11-14 09:06:35 -0500

    Created project for common Partition and Event Hub consumer contents (spanning legacy and modern EPH etc.)

---
+++ src/praxicloud.eventprocessors.tests/FixedPartitionManagerTests.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Christopher Clayton. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace praxicloud.eventprocessors.tests
+{
+    #region Using Clauses
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    #endregion
+
+    /// <summary>
+    /// Tests associated with the fixed partition manager
+    /// </summary>
+    [TestClass]
+    public class FixedPartitionManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/praxicloud.eventprocessors/IPartitionManager.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Christopher Clayton. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace praxicloud.eventprocessors
+{
+    /// <summary>
+    /// A class that handles management of partitions, including retrieving lists of available partitions
+    /// </summary>
+    public interface IPartitionManager : IOwnershipLookup
+    {
+        /// <summary>
+        /// Returns the ids of all partitions owned by the current manager
+        /// </summary>
+        /// <returns>An array of partition ids that are owned by the manager</returns>
+        string[] GetOwnedPartitions();
+
+        /// <summary>
+        /// Returns the ids of all partitions 
+        /// </summary>
+        /// <returns>An array of partition ids</returns>
+        string[] GetPartitions();
+    }
+}
